2371c1521eead28126894ac6b58f2520aac87b00
Manish kumar <manishkumar886@gmail.com>
2023-01-31 19:09:05 +0530

    my first core java program

---
+++ ForloopProgram/src/Forloop/ForLoopProgram.java
@@ -0,0 +1,26 @@
+package Forloop;
+
+public class ForLoopProgram {
+
+	public static void main(String[] args) {
+	/*System.out.println("Hello World");
+	System.out.println("Hello World");
+	System.out.println("Hello World");
+	System.out.println("Hello World");
+	System.out.println("Hello World");
+	System.out.println("Hello World");
+	*/
+		
+for(int i=0;i<10;i++) {
+	System.out.println("My Name is Manish");
+
+for(int b=0;b<11;b++) {
+	System.out.println(b);
+}
+ for(int a=10;a>0;a--) {
+	 System.out.print(a);
+ }
+for(char c1= 'A' ;c1<= 'Z';c1++) {
+	 System.out.println(c1);
+ 
+}}}}
