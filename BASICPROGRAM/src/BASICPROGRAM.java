e707f9d27dec40521a4d3147862199293e7f4488
Manish kumar <manishkumar886@gmail.com>
2023-01-31 12:15:39 +0530

    my first file access

---
+++ BASICPROGRAM/src/BASICPROGRAM.java
@@ -0,0 +1,21 @@
+
+public class BASICPROGRAM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AreaofCircle(10);
+		AreaofCircle(20);
+		AreaofCircle(40);
+	}
+		
+static void AreaofCircle(int r) {
+	double pi =3.14;
+	double	Area = pi*r*r ;
+	
+	
+	System.out.println("Area=" +Area);
+}
+	}
+
+
+
---
+++ BASICPROGRAM/src/MethodPractice.java
@@ -0,0 +1,23 @@
+
+public class MethodPractice {
+
+	public static void main(String[] args) {
+	getAverage();
+	}
+		/*getaddition();}
+		static void getaddition() {
+int x=21,y=25,res;
+res=x+y;
+System.out.println("result is="+res);
+*/
+		static void getAverage() {
+			int a=10,b=20,c=30,avg;
+			avg=(a+b+c)/3;
+			System.out.println("Num1: "+a);
+			System.out.println("Num2: "+b);
+			System.out.println("Num3: "+c);
+			System.out.println("Avg: "+avg);
+		
+	}
+}
+}
